39e412dad6289aaa327a8edbc3bdc390df01be73
Patrick Michaud <pmichaud@uw.edu>
2015-10-30 10:56:04 -0700

    adding unittest2 the right way

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ def main():
                        ],
 
         'url': 'https://github.com/mkleehammer/pyodbc',
+        'install_requires': ['unittest2'],
         'cmdclass': { 'version' : VersionCommand,
                      'tags'    : TagsCommand }
         }
